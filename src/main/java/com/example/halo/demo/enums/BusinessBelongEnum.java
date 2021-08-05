fd1475f33e6331f7d04c28cb9ca3cce5bddd6903
liuruyi <liuruyi2@tal.com>
2021-08-05 17:04:59 +0800

    新增枚举

--- src/main/java/com/example/halo/demo/enums/BusinessBelongEnum.java
+++ src/main/java/com/example/halo/demo/enums/BusinessBelongEnum.java
@@ -1,6 +1,36 @@
-package com.example.halo.demo.enums;/**
- * @Description: 
+package com.example.halo.demo.enums;
+
+import lombok.Getter;
+
+/**
+ * @Description:
  * @author: halo_ry
  * @Date: 2021/8/5 17:01
- */public class BusinessBelongEnum {
+ */
+public enum BusinessBelongEnum {
+
+    /**
+     * 代表全部，不区分业务归属
+     */
+    ALL(-1, "全部"),
+
+    /**
+     * 培优
+     */
+    PEIYOU(1, "培优"),
+
+    /**
+     * 小猴学堂
+     */
+    LITTLE_MONKEY_SCHOOL(3, "小猴学堂"),
+    ;
+    @Getter
+    private int code;
+    @Getter
+    private String desc;
+
+    BusinessBelongEnum(int code, String desc) {
+        this.code = code;
+        this.desc = desc;
+    }
 }
