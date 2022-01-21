fc4ae8914e49e73e07b6763ebd95a3f99ad0c9b6
liuruyi <liuruyi2@tal.com>
2022-01-21 15:40:53 +0800

    打印日志

--- src/main/java/com/example/halo/demo/DemoApplication.java
+++ src/main/java/com/example/halo/demo/DemoApplication.java
@@ -15,7 +15,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("DemoApplication IS START ... ");
+        System.out.println("DemoApplication START SUCCESS ... ");
     }
 
 }
